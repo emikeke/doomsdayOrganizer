8abcbabd11906bfa40b13517d737067a4f9d21cf
Emily K <emily.ke@hotmail.com>
2022-07-25 12:39:49 -0400

    Add to Github Pages.

--- src/index.js
+++ src/index.js
@@ -6,15 +6,12 @@ import App from './App';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { faTrash, faPenToSquare } from '@fortawesome/free-solid-svg-icons';
-import { HashRouter } from 'react-router-dom'
 
 library.add(fab, faTrash, faPenToSquare);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </React.StrictMode>
 );
