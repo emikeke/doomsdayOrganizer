f635179676a59b8e2b59e9473aa410bfa0b85713
Emily K <emily.ke@hotmail.com>
2022-07-25 12:27:07 -0400

    Add to Github Pages.

--- src/components/Error.js
+++ src/components/Error.js
@@ -4,7 +4,7 @@ import React from "react";
 const Error = () => {
   return (
     <div className="error-text">
-      <h1>Hi, the page you have requested is not found. Please go back to the <Link to="/doomsdayOrganizer">main page</Link>.</h1>
+      <h1>Hi, the page you have requested is not found. Please go back to the <Link to="/">main page</Link>.</h1>
     </div>
   )
 }
