3eca3de2a7c5d0d33fa972b4487d627988a786a8
Gabe <gabrieldsumabat@gmail.com>
2022-07-27 21:50:24 -0400

    DO-5: Fixed jest-environments not being setup and torn down.

--- src/components/TodoList.test.js
+++ src/components/TodoList.test.js
@@ -1,10 +1,16 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import React from "react";
 import {render} from "@testing-library/react";
 import TodoList from "./TodoList";
-import nanoid from 'nanoid/generate'
+
+
+jest.mock("nanoid", () => {
+  return {nanoid: () => "ABCD"};
+});
 
 test("should render the App landing page", () => {
-  jest.mock("nanoid");
-
   render(<TodoList/>);
 });
